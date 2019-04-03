648d3a346ce4f88702ba60f97531659ccf51a53f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-03 18:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const createFareMultiplier = function(int) {
 
 const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
-
+dfs
 function fetchSpecifiedDrivers(array, rand_function) {
   return rand_function(array)
 }
